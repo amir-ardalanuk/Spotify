a0c3becb034c8017de7f5d2acb9250f5dadc0dd3
amir.ardalanuk <amir.ardalanuk@gmail.com>
2019-07-01 19:52:58 +0430

    Fix : search item cell Component's added sub view's

--- Spotify-Sample/Screen/main/Search/Search item /SearchItemCell.swift
+++ Spotify-Sample/Screen/main/Search/Search item /SearchItemCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SearchItemCell: UITableViewCell,ReusableView {
 
+    @IBOutlet weak var imgContent: UIImageView!
+    @IBOutlet weak var lblDescribtion: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
