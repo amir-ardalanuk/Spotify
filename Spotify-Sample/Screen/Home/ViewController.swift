a1f6e2510eca5c64d886d808918e77fb6ed66b0b
amir.ardalanuk <amir.ardalanuk@gmail.com>
2019-07-01 10:37:48 +0430

    New : Added require Pods

---
+++ Spotify-Sample/Screen/Home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify-Sample
+//
+//  Created by Amir Ardalan on 7/1/19.
+//  Copyright © 2019 Amir Ardalani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
